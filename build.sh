3abce52dac63e316cd7e0dc18d3db8421e0b1e5a
blacksky3 <blacksky3@tuta.io>
2023-08-25 21:43:05 -0400

    new

--- build.sh
+++ build.sh
@@ -19,35 +19,30 @@ cd spirv/spirv-headers-git/ && makepkg -si --noconfirm && cd ${source}
 
 cd spirv/spirv-tools-git/ && makepkg -si --noconfirm && cd ${source}
 
-cd glslang/glslang-git/ && makepkg -si --noconfirm && cd ${source}
+cd spirv/lib32-spirv-tools-git/ && makepkg -si --noconfirm && cd ${source}
 
-cd llvm-minimal-git/llvm-minimal-git/ && makepkg -si --noconfirm && cd ${source}
+cd glslang-git/ && makepkg -si --noconfirm && cd ${source}
 
-cd llvm-minimal-git/libclc-minimal-git/ && makepkg -si --noconfirm && cd ${source}
+cd llvm/llvm-minimal-git/ && makepkg -si --noconfirm && cd ${source}
 
-cd llvm-minimal-git/lib32-llvm-minimal-git/ && makepkg -si --noconfirm && cd ${source}
+cd llvm/spirv-llvm-translator-minimal-git/ && makepkg -si --noconfirm && cd ${source}
 
-cd directx-headers/directx-headers-git/ && makepkg -si --noconfirm && cd ${source}
+cd llvm/libclc-minimal-git/ && makepkg -si --noconfirm && cd ${source}
+
+cd lib32-llvm/lib32-llvm-minimal-git/ && makepkg -si --noconfirm && cd ${source}
+
+cd lib32-llvm/lib32-spirv-llvm-translator-minimal-git/ && makepkg -si --noconfirm && cd ${source}
+
+cd directx-headers-git/ && makepkg -si --noconfirm && cd ${source}
 
 cd libdrm/libdrm-git/ && makepkg -si --noconfirm && cd ${source}
 
 cd libdrm/lib32-libdrm-git/ && makepkg -si --noconfirm && cd ${source}
 
-cd libglvnd/libglvnd-git/ && makepkg -si --noconfirm && cd ${source}
-
-cd libglvnd/lib32-libglvnd-git/ && makepkg -si --noconfirm && cd ${source}
-
-cd mesa/mesa-git/ && makepkg -si --noconfirm && cd ${source}
-
-cd mesa/lib32-mesa-git/ && makepkg -si --noconfirm && cd ${source}
+cd mesa/mesa/ && makepkg -si --noconfirm && cd ${source}
 
 # make a copy of every pkg in package/ dir
 
 mkdir package
 
 cp -v */*/*.pkg.tar.zst package/
-
-# clean build dir
-
-rm -rf */*/src/
-rm -rf */*/pkg/
