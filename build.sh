8f0499c69b8fe07193f6ca4a507c6ffcaf97b51b
archdevlab <146228772+archdevlab@users.noreply.github.com>
2023-10-12 02:25:25 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,8 +24,6 @@ cd llvm/libclc-minimal-git/ && makepkg -si --noconfirm && cd ${source}
 
 cd lib32-llvm/lib32-llvm-minimal-git/ && makepkg -si --noconfirm && cd ${source}
 
-cd lib32-llvm/lib32-clang-minimal-git/ && makepkg -si --noconfirm && cd ${source}
-
 cd lib32-llvm/lib32-spirv-llvm-translator-minimal-git/ && makepkg -si --noconfirm && cd ${source}
 
 cd directx-headers-git/ && makepkg -si --noconfirm && cd ${source}
